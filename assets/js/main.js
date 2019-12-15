61758dc7509bec7ecc069b6f729e87b5034083e3
Xizted <xizted79@gmail.com>
2019-12-15 19:54:09 -0400

    Bug Fix Animation

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,6 +3,12 @@ window.onload = function(){
     $('body').removeClass('hidden-body');
     $('nav').addClass('fixed-top');
 
+    $('.promo-title').addClass('animated fadeInUp');
+    $('.promo-subtitle').addClass('animated fadeInLeft');
+    $('.btn-downloadnow').addClass('animated delay-1s zoomInLeft');
+    $('.btn-learnmore').addClass('animated delay-1s zoomInRight');
+    $('.smartphone').addClass('animated bounceInUp');
+
     new WOW().init();
 }
 
