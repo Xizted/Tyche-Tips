486f810dd22b283f1570e908272f21d3393bc02f
Xizted <xizted79@gmail.com>
2019-12-15 20:33:20 -0400

    Add CDN

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,13 @@
-window.onload = function(){
-    $('#preloader').fadeOut();
-    $('body').removeClass('hidden-body');
-    $('nav').addClass('fixed-top');
+window.onload = function() {
+  $("#preloader").fadeOut();
+  $("body").removeClass("hidden-body");
+  $("nav").addClass("fixed-top");
 
-    $('.promo-title').addClass('animated fadeInUp');
-    $('.promo-subtitle').addClass('animated fadeInLeft');
-    $('.btn-downloadnow').addClass('animated delay-1s zoomInLeft');
-    $('.btn-learnmore').addClass('animated delay-1s zoomInRight');
-    $('.smartphone').addClass('animated bounceInUp');
+  $(".promo-title").addClass("animated fadeInUp");
+  $(".promo-subtitle").addClass("animated fadeInLeft");
+  $(".btn-downloadnow").addClass("animated delay-1s zoomInLeft");
+  $(".btn-learnmore").addClass("animated delay-1s zoomInRight");
+  $(".smartphone").addClass("animated bounceInUp");
 
-    new WOW().init();
-}
-
+  new WOW().init();
+};
