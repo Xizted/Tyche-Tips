def16d2d1b3345b8ed362f1efd1096023a618090
Xizted <xited79@gmail.com>
2020-01-06 20:47:41 -0400

    Text in spanish, Add Logo and NavbarModified

--- V1/assets/js/main.js
+++ V1/assets/js/main.js
@@ -6,14 +6,6 @@ window.onload = function() {
   $(".btn-downloadnow").addClass("animated delay-1s zoomInLeft");
   $(".btn-learnmore").addClass("animated delay-1s zoomInRight");
   $(".smartphone").addClass("animated bounceInUp");
-
-  $(document).on("click", ".download-btn", function() {
-    $(this).fadeIn(function() {
-      $(this)
-        .addClass("hover-active")
-        .siblings()
-        .removeClass("hover-active");
-    });
-  });
+  
   new WOW().init();
 };
